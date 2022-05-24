d1716eb63b862995ba32319f0a8937216330ab30
João Vitor Felix de Andrade <joao.felixandrade1201@gmail.com>
2022-05-24 15:44:42 -0300

    Criando projeto no React

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img className='img' src="https://i.imgur.com/xdzqr2q.jpg" alt="Levi beyblade" />
+        </>
+    )
+}
+
+export default Home
