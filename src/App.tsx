0ef3357527ee4e6fd87d7037deb64cb05d50fb81
João Vitor Felix de Andrade <joao.felixandrade1201@gmail.com>
2022-05-25 14:06:10 -0300

    Criando e estilizando os primeiros componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import NavBar from './components/statics/navbar/NavBar';
+import Footer from './components/statics/footer/Footer';
 import Home from './pages/Home/Home'
 import './App.css';
 
 function App() {
-  return(
-    <Home />
+  return (
+    <>
+      <NavBar />
+      <Home />
+      <Footer />
+    </>
   )
 }
 
