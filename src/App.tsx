20f60d64782a592682b71066ffb14e5274374085
João Vitor Felix de Andrade <joao.felixandrade1201@gmail.com>
2022-05-27 18:53:35 -0300

    Criação e estilização do Login e Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/statics/navbar/NavBar';
 import Footer from './components/statics/footer/Footer';
 import Home from './pages/Home/Home'
+import Login from './pages/Login/Login';
+
 import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-      <Home />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </Router>
   )
 }
 
